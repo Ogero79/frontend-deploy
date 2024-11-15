b106016a13ce0010af45bd69221602459baa9839
Ogero79 <brianogero@kabarak.ac.ke>
2024-11-15 15:48:44 +0300

    made change to accomodate vite plugin

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default {
-  base: '/frontend-deployed/',  // Replace with your repo name
-};
+export default defineConfig({
+  plugins: [react()],
+  base: '/frontend-deployed/',  // Use your actual repo name here
+})
